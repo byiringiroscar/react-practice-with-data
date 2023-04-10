6bce79ab28d2ed6b283d044e41ad39d2dbd59e47
byiringiroscar <byiringoroscar@gmail.com>
2023-04-10 15:53:47 +0200

    Add section for free movies with An API

--- src/components/free/freeMovieContainer.js
+++ src/components/free/freeMovieContainer.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleMoviesFree from './singleMoviesFree';
 
-const ContainerMoviesFree = () => (
-  <div className="container-movie-free">
-    <SingleMoviesFree />
-    <SingleMoviesFree />
-    <SingleMoviesFree />
-  </div>
-);
+const ContainerMoviesFree = () => {
+  const [movies, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await fetch('https://api.tvmaze.com/shows');
+        const data = await response.json();
+        // const limitedData = data.slice(0, 24);
+        // console.log(limitedData);
+        setMovie(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    getMovies();
+  }, []);
+  return (
+    <div className="container-movie-free">
+      {loading && <img src="https://media.tenor.com/UnFx-k_lSckAAAAM/amalie-steiness.gif" alt="loading" />}
+      {error && <img src="https://media.tenor.com/hwe3vkln0_UAAAAM/error-x-error.gif" alt="loading" />}
+      {!loading && !error && movies.map((movie) => (
+        <SingleMoviesFree key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
 
 export default ContainerMoviesFree;
--- src/components/free/singleMoviesFree.js
+++ src/components/free/singleMoviesFree.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SingleMoviesFree = () => (
+const SingleMoviesFree = ({ movie }) => (
   <div className="free-movie">
-    <img src="https://m.timesofindia.com/thumb.cms?msid=5348868&width=200&height=281" alt="movieImage" />
-    <a href="/"><img className="play" src="https://cdn-icons-png.flaticon.com/512/148/148744.png" alt="play" /></a>
+    <img src={movie.image.medium} alt="movieImage" />
+    <a target="_blank" href={movie.url} rel="noreferrer"><img className="play" src="https://cdn-icons-png.flaticon.com/512/148/148744.png" alt="play" /></a>
   </div>
 );
+
+SingleMoviesFree.propTypes = {
+  movie: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default SingleMoviesFree;
