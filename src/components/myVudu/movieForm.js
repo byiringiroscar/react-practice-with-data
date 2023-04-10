d2d132bb6798fe42fc859a33ee93b9b97302a37e
byiringiroscar <byiringoroscar@gmail.com>
2023-04-10 13:38:51 +0200

    Add required to the form and add type url

--- src/components/myVudu/movieForm.js
+++ src/components/myVudu/movieForm.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 const MyForm = ({ handleSubmit, handleInputChange, input }) => (
   <form onSubmit={handleSubmit}>
-    <input type="text" value={input.movieName} onChange={handleInputChange} name="movieName" placeholder="Enter movie name" />
-    <input type="text" value={input.movieImage} onChange={handleInputChange} name="movieImage" placeholder="Enter movie image url" />
+    <input type="text" value={input.movieName} onChange={handleInputChange} name="movieName" placeholder="Enter movie name" required />
+    <input type="url" value={input.movieImage} onChange={handleInputChange} name="movieImage" placeholder="Enter movie image url" required />
     <button type="submit">Add Movie</button>
   </form>
 );
